9f75e1028246ddfa1e5e31955997df837b86976b
Luuk <info@luuk.site>
2022-10-27 13:54:55 +0200

    Update to Next 13

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <Link href="/pages/test/">
-      <a>
-        <h1 style={{ textAlign: "center" }}>Visit this link</h1>
-      </a>
+      <h1 style={{ textAlign: "center" }}>Visit this link</h1>
     </Link>
   );
 }
